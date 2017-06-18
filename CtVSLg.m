f488246403844fb39593da5838b84adfb77258fa
Tim Hills <tim.hills@hotmail.com>
2017-06-17 19:05:36 -0600

    created: Sat Jun 17 19:05:36 MDT 2017

---
+++ CtVSLg.m
@@ -0,0 +1,107 @@
+classdef CtVSLg < handle
+    %UNTITLED6 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        DistanceFromCentroidArray
+        DistanceFromLeadGooseArray
+        FigureAnalyzed
+        AnalysisDate
+        LeadGoose
+        Centroid
+        GeesePositionArray
+        NumOfGeese
+    end
+    
+    methods
+        
+        function obj = CtVSLg(Date,FileName)
+            obj.AnalysisDate = Date;
+            obj.FigureAnalyzed = FileName;
+            obj.DistanceFromCentroidArray = [];
+            obj.DistanceFromLeadGooseArray = [];
+        end
+        
+        function ExtractData(obj)
+            % Extract centroid data of a flock
+            obj.FindLeadGoose();       
+            [Population,X,Y,Z] = GetGraphData1(obj.FigureAnalyzed);
+            obj.GeesePositionArray = [X;Y;Z];
+            obj.NumOfGeese = Population;
+            
+        end
+        
+        function FindLeadGoose(obj)
+            % lead goose has a special LineWidth property
+            FindObj1 = findobj(gca,'LineWidth',5);
+            obj.LeadGoose = [FindObj1.XData;FindObj1.YData;FindObj1.ZData];
+            
+        end
+        
+        function CalculateCentroid(obj)
+            % arithmetic mean of position data
+            XCentroid = mean(obj.GeesePositionArray(1,:));
+            YCentroid = mean(obj.GeesePositionArray(2,:));
+            ZCentroid = mean(obj.GeesePositionArray(3,:));
+            obj.Centroid = [XCentroid;YCentroid;ZCentroid];
+            
+        end
+        
+        function CalculateDistanceFromCentroid(obj)
+            % Populate the data array that contains each flock member's
+            % distance away from the flock's centroid
+            for i = 1:obj.NumOfGeese
+               CtDistance = sqrt(sum((obj.GeesePositionArray(:,i) - obj.Centroid).^2));
+               obj.DistanceFromCentroidArray = [obj.DistanceFromCentroidArray,...
+                   CtDistance];
+            end
+            
+        end
+        
+        function CalculateDistanceFromLeadGoose(obj)
+            % Populate the data array that contains each flock member's
+            % distance away from the flock's centroid
+            for i = 1:obj.NumOfGeese
+               CtDistance = sqrt(sum((obj.GeesePositionArray(:,i) - obj.LeadGoose).^2));
+               obj.DistanceFromLeadGooseArray = [obj.DistanceFromLeadGooseArray,...
+                   CtDistance];
+            end
+            
+        end
+        
+        function SortData(obj)
+            % Sort obj.DistanceFromLeadGooseArray from smallest value to
+            % its largest value and organize it's corresponding
+            % obj.DistanceFromCentroidArray value according to the same
+            % index.
+            [obj.DistanceFromLeadGooseArray,OrganizedIndexArray] = sort(obj.DistanceFromLeadGooseArray);
+            
+            % Reorganize obj.DistanceFromCentroidArray
+            TempCentroidDistanceArray = [];
+            
+            for DataPosition = 1:obj.NumOfGeese 
+                
+                TempCentroidDistanceArray = [TempCentroidDistanceArray,...
+                    obj.DistanceFromCentroidArray(OrganizedIndexArray(DataPosition))];
+                
+            end
+            
+            % Reset obj.DistanceFromCentroidArray
+            obj.DistanceFromCentroidArray = TempCentroidDistanceArray;
+            
+        end
+        
+        function GenerateFigure(obj,FigNum)
+            % Plot each flock member's distance from the flock's centroid
+            % as a function of that same member's distance from the lead
+            % goose
+            figure(FigNum);
+            plot(obj.DistanceFromLeadGooseArray,obj.DistanceFromCentroidArray,...
+                '-ob','LineWidth',2);
+            
+        end
+        
+    end
+    
+end
+
---
+++ CtVSLgTestScript.m
@@ -0,0 +1,89 @@
+% Distance from Centroid vs distance from lead goose functional
+% Author: Tim Hills
+% Made: 6-17-2017
+
+clear all; close all; clc
+
+% user selects which dir the geese figures are in
+FigDir = uigetdir('C:\Users\Timothy\Dropbox\Research\', ...
+    'Choose the directory in which the geese centroid figures are.');
+
+% user selects which dir to save data
+SaveDir = uigetdir('C:\Users\Timothy\Dropbox\Research',...
+    'Choose the directoy you would like to save CtVSLg data.');
+
+% included in name of generated .xlsx file
+AnalysisDate = datestr(now,'mm_dd_yyyy');
+
+% set cd to get data from .fig files
+cd(FigDir);
+
+FileObj = dir('*.fig')';
+
+for i = 1:length(FileObj)
+    
+    % set cd to get data from .fig files
+    cd(FigDir);
+    
+    DynamicFile = FileObj(i);
+    MyCtVSLg = CtVSLg(AnalysisDate,DynamicFile);
+    openfig(DynamicFile.name,'invisible');
+    
+    % Extract data from centroid figure
+    MyCtVSLg.ExtractData();
+    
+    % Find the lead good of the flock
+    MyCtVSLg.FindLeadGoose();
+    
+    % Derive the arithmetic mean of a flock based of it's members centroid
+    % position
+    MyCtVSLg.CalculateCentroid();
+    
+    % Calculate the distance of each flock member's distance from centroid
+    % as well as that same member's distance from the lead goose
+    MyCtVSLg.CalculateDistanceFromCentroid();
+    MyCtVSLg.CalculateDistanceFromLeadGoose();
+    
+    % Organize data
+    MyCtVSLg.SortData();
+    
+    % Set each data array to more managable variable names
+    Ct = MyCtVSLg.DistanceFromCentroidArray;
+    Lg = MyCtVSLg.DistanceFromLeadGooseArray;
+    
+    % Close original figure
+    close all
+
+    % cd to save data
+    cd(SaveDir);
+    
+    % Set .fig name
+    SetFigName(DynamicFile.name);
+    
+    % Plot Ct as a function of Lg
+    MyCtVSLg.GenerateFigure(i);
+    
+    % Get a handle on the current figure
+    CurrentFigObj = get(gcf);
+    
+    % save figure
+    SaveFig(SaveDir,DynamicFile.name);
+    
+    %writes an Excel spreadsheet of the packing factors to the current folder
+    xlswrite(strrep(DynamicFile.name,'.fig',''),{'Distance_from_centroid'},...
+            sprintf('Sheet%s',num2str(i)),sprintf('A%d',1))
+        
+    xlswrite(strrep(DynamicFile.name,'.fig',''),{'Distance_from_lead_goose'},...
+        sprintf('Sheet%s',num2str(i)),sprintf('B%d',1))
+    
+    for j = 1:length(Ct)
+        
+        xlswrite(strrep(DynamicFile.name,'.fig',''),Ct(j),...
+            sprintf('Sheet%s',num2str(i)),sprintf('A%d',j+1))
+        
+        xlswrite(strrep(DynamicFile.name,'.fig',''),Lg(j),...
+            sprintf('Sheet%s',num2str(i)),sprintf('B%d',j+1))
+        
+    end
+    
+end
